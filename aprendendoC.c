6929360c4d8f6e5b01ef51e84e27a22c306ef88d
dinorahfariasc <dinorahfarias@outlook.com>
2023-08-17 20:59:59 -0300

    commit

---
+++ aprendendoC.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // entrada de dados e especificadores 
+    
+    printf("Hello, world!\n");
+    printf("oiiiiiiiii!\n");
+    printf("oi tenho 19 anos!\n");
+    printf("valor inteiro: %d.\n",10);
+    printf("valor real: %f. \n", 3.14159265);
+    printf("valor real com apenas duas casas: %.2f. \n",3.141592);
+    printf("dado de texto: %c. \n",'a');
+    printf("dado de texto: %s.\n","testando");
+
+    //system("pause"); // se for rodar pelo win o sistema executa e ja fecha o programa, aq pausamos para ver o resultado
+    // alterar o codigo tem que recompilar
+
+
+
+}
